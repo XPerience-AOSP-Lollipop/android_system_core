MotorolaMobilityLLC/system-core

AoV: Extend firmware loading folder
AoV with Cirrus XMCS codec relies on loading the model and
the firmware by using kernel firmware class facility.
This patch extends current list of firmware folders to include
one under /data partition so that dynamically generated model or
downloaded firmware can be still picked up when needed.

Change-Id: Id1ba04c63866a302b735b1041979424a078042db
Signed-off-by: Kazuhiro Ondo <ondo@motorola.com>
Reviewed-on: http://gerrit.mot.com/721996
SLTApproved: Slta Waiver <sltawvr@motorola.com>
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Joel Voss <jvoss@motorola.com>
Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

init/Android.mk
@@ -59,6 +59,11 @@ ifeq ($(strip $(TARGET_USE_MOT_NEW_COM)),true)
 LOCAL_CFLAGS    += -DMOTO_NEW_CHARGE_ONLY_MODE
 endif
 
+# IKVOICE-4341 - Extend firmware loading folder list if XMCS codec is used for AOV
+ifeq ($(BOARD_HAS_AUDIO_DSP_XMCS),true)
+LOCAL_CFLAGS    += -DMOTO_AOV_WITH_XMCS
+endif
+
 # Enable ueventd logging
 #LOCAL_CFLAGS += -DLOG_UEVENTS=1
 ifdef DOLBY_UDC_MULTICHANNEL

XPerience-AOSP-Lollipop/android_system_core

Signed-off-by: klozz <klozz707@gmail.com>
Conflicts:
	init/Android.mk

init/Android.mk
@@ -20,6 +20,11 @@ ifneq ($(TARGET_INIT_CONSOLE_TIMEOUT),)
 init_options += -DCONSOLE_TIMEOUT_SEC=$(TARGET_INIT_CONSOLE_TIMEOUT)
 endif
 
+# IKVOICE-4341 - Extend firmware loading folder list if XMCS codec is used for AOV
+ifeq ($(BOARD_HAS_AUDIO_DSP_XMCS),true)
+init_options    += -DMOTO_AOV_WITH_XMCS
+endif
+
 init_cflags += \
     $(init_options) \
     -Wall -Wextra \